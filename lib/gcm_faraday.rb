401631202bb6a42efa9f9c5c62e82c98ad6500f4
Trevor John <trevor@john.tj>
2014-09-21 17:01:41 -0400

    add GcmFaraday::Middleware::Authentication for injecting auth key in header

--- lib/gcm_faraday.rb
+++ lib/gcm_faraday.rb
@@ -2,6 +2,7 @@ require 'faraday'
 require 'gcm_faraday/version'
 require 'gcm_faraday/middleware'
 require 'gcm_faraday/middleware/canonical_id'
+require 'gcm_faraday/middleware/authentication'
 
 module GcmFaraday
   # Your code goes here...
---
+++ lib/gcm_faraday/middleware/authentication.rb
@@ -0,0 +1,23 @@
+module GcmFaraday
+  module Middleware
+    class Authentication
+      def initialize(app, options = {})
+        @app, @options = app, options
+      end
+
+      def call(env)
+        env.request_headers['Authorization'] = auth_key
+
+        app.call(env).on_complete { |env| }
+      end
+
+      private
+
+      attr_reader :app, :options
+
+      def auth_key
+        @auth_key ||= "key=#{options[:key]}".freeze
+      end
+    end
+  end
+end
